f231a6140ac8fe61ed0e3ac84a8f0a02a0100697
Jon Roby <jonathan.roby1@gmail.com>
2016-07-12 13:48:50 -0700

    setup config for dev and prod environments

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var path = require('path');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer();
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+var publicPath = path.resolve(__dirname, 'public');
 
-const port = process.env.PORT || 8000;
-app.listen(port, function() {
+app.use(express.static(publicPath));
+
+if (!isProduction) {
+  var bundle = require('./server/bundle.js');
+  bundle();
+
+  app.all('/build/*', (req, res) => {
+    proxy.web(req, res, {
+      target: 'http://localhost:8080'
+    });
+  });
+}
+
+proxy.on('error', (e) => {
+  console.log('Could not connect to proxy, please try again');
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
---
+++ server/bundle.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var fs = require('fs');
+var Webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+
+var webpackConfig = require('./../webpack.config.js');
+var mainPath = path.resolve(__dirname, '..', 'app', 'main.js');
+
+module.exports = () => {
+  var bundleStart = null;
+  var compiler = Webpack(webpackConfig);
+
+  compiler.plugin('compile', () => {
+    console.log('Bundling...');
+    bundleStart = Date.now();
+  });
+
+  compiler.plugin('done', () => {
+    console.log(`Bundled in ${Date.now() - bundleStart} + ms!`);
+  });
+
+  var bundler = new WebpackDevServer(compiler, {
+    publicPath: '/build/',
+
+    hot: true,
+
+    quiet: false,
+    noInfo: true,
+    stats: {
+      colors: true
+    }
+  });
+
+  bundler.listen(8080, 'localhost', () => {
+    console.log('Bundling project, please wait...');
+  });
+}
